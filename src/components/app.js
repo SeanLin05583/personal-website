2d3e8e7c096aeceec10155feb2912bae5de15ce8
Sean Lin <38515565+SeanLin05583@users.noreply.github.com>
2019-02-25 22:57:26 +0800

    add plugin-proposal-export-default-from package / add basic home page

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import {Home} from './pages/homePage';
 
 class App extends Component {
 	render() {
 		return (
 			<h1>
 				test React
+				<Home />
 			</h1>
 		);
 	}
---
+++ src/components/pages/homePage/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.css';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className={style.home}>
+        home Page
+      </div>
+    );
+  }
+}
