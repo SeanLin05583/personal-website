e5478779a3abf0536b1b7d8975e606f96c7ec3da
Sean Lin <38515565+SeanLin05583@users.noreply.github.com>
2019-02-25 22:56:34 +0800

    remove ghpage template / add several css loader / add uglifyjs-webpack-plugin, autoprefixer package

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+
 class App extends Component {
 	render() {
-		return (<h1>My React App</h1>)
+		return (
+			<h1>
+				test React
+			</h1>
+		);
 	}
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,62 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: "html-loader"
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader' },
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+							localIdentName: '[name]-[local]-[hash:base64:8]',
+						},
+					},
+					{ loader: 'postcss-loader' },
+				]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					{ loader: 'style-loader' },
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+							localIdentName: '[name]-[local]-[hash:base64:8]',
+						},
+					},
+					{ loader: 'postcss-loader' },
+					{ loader: 'sass-loader' },
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html"
+		}),
+	],
+	optimization: {
+    minimizer: [new UglifyJsPlugin()],
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
 };
