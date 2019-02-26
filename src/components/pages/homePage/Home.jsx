9fbe520c53677d4958b1851cce818e31129479a8
Sean Lin <38515565+SeanLin05583@users.noreply.github.com>
2019-02-27 00:59:01 +0800

    add canvas-nest

--- src/components/pages/homePage/Home.jsx
+++ src/components/pages/homePage/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import CanvasNest from 'canvas-nest.js';
 import Banner from './Banner.jsx';
+import classNames from 'classnames/bind';
+import style from './style.css';
+
+const cx = classNames.bind(style);
+
+const config = {
+  color: '25,172,255',
+  count: 80,
+};
 
 export default class Home extends React.Component {
+  componentDidMount() {
+    new CanvasNest(this.main, config);
+  }
   render() {
     return (
       <div>
         <Banner />
+        <div
+          ref={ref => this.main = ref}
+          className={cx('intro')}
+        >
+        </div>
       </div>
     );
   }
