01a0d7f371bc81fb4bce65cef5685bd1054092f0
Sean Lin <38515565+SeanLin05583@users.noreply.github.com>
2019-02-27 00:26:02 +0800

    add fontawesome cdn / add mobile header

--- src/components/pages/homePage/Banner.jsx
+++ src/components/pages/homePage/Banner.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import style from './style.css';
 
-const cssClass = classNames.bind(style);
+const cx = classNames.bind(style);
 
 export default class Banner extends React.Component {
   render() {
     return (
       <header>
-        <div className={cssClass('banner-container')}>
-          <img className={cssClass('banner-img')} />
-          <div className={cssClass('banner-title-container')}>
-            <h1 className={cssClass('banner-title')}>SeanLin Profile</h1>
-            <ul className={cssClass('banner-menu')}>
-              <li>Skill</li>
-              <li>Experience</li>
-              <li>Contact</li>
-            </ul>
+        <img className={cx('banner-img')} />
+        <div className={cx('banner-title-container')}>
+          <h1 className={cx('banner-title')}>SeanLin's Profile</h1>
+          <ul className={cx('banner-menu')}>
+            <li>Skill</li>
+            <li>Experience</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className={cx('header')}>
+          <span>SeanLin's Profile</span>
+          <div className={cx('menu-icon')}>
+            <i className="fas fa-bars" />
           </div>
         </div>
       </header>
