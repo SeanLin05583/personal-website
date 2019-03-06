08a847b931ac06fa115687cefaa7c00f55ab4e35
Sean Lin <38515565+SeanLin05583@users.noreply.github.com>
2019-03-07 00:23:13 +0800

    update leo logo / adjust experience layout

--- src/components/pages/homePage/Intro/Experience.jsx
+++ src/components/pages/homePage/Intro/Experience.jsx
@@ -2,49 +2,58 @@ import React, { Component } from 'react';
 import classNames from 'classnames/bind';
 import style from './style.css';
 
+const imgAccupass = require('../../../../assets/img/logo-accupass.png');
+const imgLeo = require('../../../../assets/img/logo-leo.png');
+
 const cx = classNames.bind(style);
 
+const expList = [
+  {
+    title: '2018.06 - 現今 Accupass',
+    img: imgAccupass,
+    imgCircle: true,
+  },
+  {
+    title: '2017.02 - 2018.06 國眾電腦',
+    img: imgLeo,
+    imgCircle: false,
+  },
+];
+
 export default class Experience extends Component {
+  renderExperiences = () => {
+    return expList.map((exp, index) =>
+      <div key={`${index}${exp.title}`} className={cx('exp')}>
+        <div className={cx('exp-logo-container')}>
+          <img className={cx('exp-logo', exp.imgCircle && 'img-circle')} src={exp.img} />
+        </div>
+        <div className={cx('exp-progress-bar-container')}>
+          <span />
+          <span />
+          <span />
+        </div>
+        <section className={cx('exp-content-container')}>
+          <div className={cx('exp-title')}>
+            <span />
+            <h3>{exp.title}</h3>
+          </div>
+          <div className={cx('exp-content')}>
+            <p>test</p>
+            <p>test</p>
+            <p>test</p>
+            <p>test</p>
+            <p>test</p>
+            <p>test</p>
+          </div>
+        </section>
+      </div>
+    );
+  }
   render() {
     return (
       <div className={cx('exp-container')}>
         <h2>Experiences</h2>
-        <div className={cx('exp')}>
-          <div className={cx('exp-logo-container')}>
-            <img className={cx('exp-logo')} src={require('../../../../assets/img/logo-accupass.png')} />
-          </div>
-          <div className={cx('exp-progress-bar-container')}>
-            <span />
-            <span />
-            <span />
-          </div>
-          <section className={cx('exp-content-container')}>
-            <div className={cx('exp-title')}>
-              <span />
-              <h3>Accupass 活動通</h3>
-            </div>
-            <div className={cx('exp-content')}>
-            </div>
-          </section>
-        </div>
-        <div className={cx('exp')}>
-          <div className={cx('exp-logo-container')}>
-            <img className={cx('exp-logo')} src={require('../../../../assets/img/logo-leo.jpg')} />
-          </div>
-          <div className={cx('exp-progress-bar-container')}>
-            <span />
-            <span />
-            <span />
-          </div>
-          <section className={cx('exp-content-container')}>
-            <div className={cx('exp-title')}>
-              <span />
-              <h3>國眾電腦</h3>
-            </div>
-            <div className={cx('exp-content')}>
-            </div>
-          </section>
-        </div>
+        {this.renderExperiences()}
       </div>
     )
   }
