89dce9f8d1946635e9f574ee128b255794ffa07f
Sean Lin <38515565+SeanLin05583@users.noreply.github.com>
2019-08-19 00:03:53 +0800

    Fix react-intl IE11 bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,14 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import logger from 'redux-logger';
 import reducers from 'reducers';
+import { addLocaleData } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import zh from 'react-intl/locale-data/zh';
 import Home from 'components/pages/homePage';
 import './style.css';
 
 const store = createStore(reducers, applyMiddleware(logger));
+addLocaleData([...en, ...zh]);
 
 export default class App extends Component {
 	render() {
