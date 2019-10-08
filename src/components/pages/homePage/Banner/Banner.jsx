e8307315e64ccad454398406f6c094fbd3968a8d
Sean Lin <seanlin05583@gmail.com>
2019-10-08 15:40:24 +0800

    [Banner] Use hooks

--- src/components/pages/homePage/Banner/Banner.jsx
+++ src/components/pages/homePage/Banner/Banner.jsx
@@ -1,42 +1,46 @@
-import React from 'react';
+import React, { forwardRef, useRef, useImperativeHandle } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { LangSelector } from 'components/common';
 import classNames from 'classnames/bind';
 import style from './style.css';
 
 const cx = classNames.bind(style);
-export default class Banner extends React.Component {
-  getBannerHeight = () => {
-    return this.bannerContainer.getBoundingClientRect().height;
-  }
 
-  getBannerBottom = () => {
-    return this.bannerContainer.getBoundingClientRect().bottom;
-  }
+const Banner = forwardRef((props, ref) => {
+  const bannerContainer = useRef(null);
+  const { onScrollToBlock, blockTitleList } = props;
 
-  render() {
-    const { onScrollToBlock, blockTitleList } = this.props;
-    return (
-      <div className={cx('banner-container')} ref={ref => this.bannerContainer = ref}>
-        <img className={cx('banner-img-web')} src={require('../../../../assets/img/banner_web.png')} />
-        <img className={cx('banner-img-mobile')} src={require('../../../../assets/img/banner_mobile.png')} />
-        <div className={cx('banner-filter')} />
-        <div className={cx('banner-title-container')}>
-          <h1 className={cx('banner-title')}>
-            <FormattedMessage id="profile.title" />
-          </h1>
-        </div>
-        <div className={cx('banner-menu-container')}>
-          <ul className={cx('banner-menu')}>
-            {blockTitleList.map(blockObj =>
-              <li key={blockObj.key} onClick={onScrollToBlock(blockObj.key)}>
-                <FormattedMessage id={blockObj.intlId} />
-              </li>
-            )}
-          </ul>
-          <LangSelector />
-        </div>
+  useImperativeHandle(ref, () => ({
+    getBannerHeight: () => {
+      return bannerContainer.current.getBoundingClientRect().height;
+    },
+    getBannerBottom: () => {
+      return bannerContainer.current.getBoundingClientRect().bottom;
+    }
+  }));
+
+  return (
+    <div className={cx('banner-container')} ref={bannerContainer}>
+      <img className={cx('banner-img-web')} src={require('../../../../assets/img/banner_web.png')} />
+      <img className={cx('banner-img-mobile')} src={require('../../../../assets/img/banner_mobile.png')} />
+      <div className={cx('banner-filter')} />
+      <div className={cx('banner-title-container')}>
+        <h1 className={cx('banner-title')}>
+          <FormattedMessage id="profile.title" />
+        </h1>
       </div>
-    );
-  }
-}
+      <div className={cx('banner-menu-container')}>
+        <ul className={cx('banner-menu')}>
+          {blockTitleList.map(blockObj =>
+            <li key={blockObj.key} onClick={onScrollToBlock(blockObj.key)}>
+              <FormattedMessage id={blockObj.intlId} />
+            </li>
+          )}
+        </ul>
+        <LangSelector />
+      </div>
+    </div>
+  );
+});
+
+export default Banner;
