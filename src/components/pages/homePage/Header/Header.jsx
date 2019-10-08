7e5a9a996555a8e2e30c815e18c077fedf4c1368
Sean Lin <seanlin05583@gmail.com>
2019-10-08 16:09:16 +0800

    [Header] Use hook

--- src/components/pages/homePage/Header/Header.jsx
+++ src/components/pages/homePage/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { NavButton, LangSelector } from 'components/common';
 import classNames from 'classnames/bind';
@@ -6,86 +6,73 @@ import style from './style.css';
 
 const cx = classNames.bind(style);
 
-export default class Header extends PureComponent {
-  state = {
-    isMenuOpen: false,
+const Header = (props) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { isHeaderFixed, isMobile, onScrollToBlock, blockTitleList } = props;
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [isMobile, isHeaderFixed]);
+
+  const handleNavClick = () => {
+    setIsMenuOpen(prevState => !prevState);
   }
 
-  static defaultProps = {
-    isHeaderFixed: false,
-  }
-
-  componentDidUpdate(prevProp) {
-    const { isHeaderFixed, isMobile } = this.props;
-    const { isMenuOpen } = this.state;
-    if (prevProp.isHeaderFixed !== isHeaderFixed && !isHeaderFixed) {
-      this.setState({ isMenuOpen: false });
-    }
-
-    if (prevProp.isMobile && !isMobile && isMenuOpen) {
-      this.setState({ isMenuOpen: false });
-    }
-  }
-
-  handleNavClick = () => {
-    const { isMenuOpen } = this.state;
-    this.setState({ isMenuOpen: !isMenuOpen });
-  }
-
-  handleMenuItemClick = (targetBlock) => () => {
-    const { onScrollToBlock } = this.props;
+  const handleMenuItemClick = (targetBlock) => () => {
     onScrollToBlock(targetBlock)();
-    this.setState({ isMenuOpen: false });
+    setIsMenuOpen(false);
   }
 
-  render() {
-    const { isMenuOpen } = this.state;
-    const { isHeaderFixed, isMobile, onScrollToBlock, blockTitleList } = this.props;
-    return (
-      <header className={cx('header-container', isHeaderFixed && 'fixed')}>
-        <div className={cx('header', isMenuOpen && 'open')}>
-          <h1 className={cx('header-title')}>
-            <FormattedMessage id="profile.title" />
-          </h1>
-          {!isMobile &&
-            <div className={cx('header-web-nav-container')}>
-              {blockTitleList.map(blockObj =>
-                <div
-                  key={blockObj.key}
-                  className={cx('header-web-nav-item')}
-                  onClick={onScrollToBlock(blockObj.key)}
-                >
-                  <span className={cx('header-web-nav-block-name')}>
-                    <FormattedMessage id={blockObj.intlId} />
-                  </span>
-                </div>
-              )}
-              <div className={cx('header-lang-selector-container')}>
-                <LangSelector isShowPadding={false} />
-              </div>
-            </div>
-          }
-          {isMobile &&
-            <div className={cx('header-mobile-left')}>
-              <div className={cx('header-mobile-lang-selector-container')}>
-                <LangSelector isShowPadding={false} />
-              </div>
-              <NavButton isOpen={isMenuOpen} onClick={this.handleNavClick} />
-            </div>
-          }
-        </div>
-        {isMobile &&
-          <nav className={cx('header-mobile-menu', isMenuOpen && 'is-open')}>
-            <ul>
-              {blockTitleList.map(blockObj =>
-                <li key={blockObj.key} onClick={this.handleMenuItemClick(blockObj.key)}>
+  return (
+    <header className={cx('header-container', isHeaderFixed && 'fixed')}>
+      <div className={cx('header', isMenuOpen && 'open')}>
+        <h1 className={cx('header-title')}>
+          <FormattedMessage id="profile.title" />
+        </h1>
+        {!isMobile &&
+          <div className={cx('header-web-nav-container')}>
+            {blockTitleList.map(blockObj =>
+              <div
+                key={blockObj.key}
+                className={cx('header-web-nav-item')}
+                onClick={onScrollToBlock(blockObj.key)}
+              >
+                <span className={cx('header-web-nav-block-name')}>
                   <FormattedMessage id={blockObj.intlId} />
-                </li>
-              )}
-            </ul>
-          </nav>
+                </span>
+              </div>
+            )}
+            <div className={cx('header-lang-selector-container')}>
+              <LangSelector isShowPadding={false} />
+            </div>
+          </div>
         }
-      </header>
-    )
-  }
+        {isMobile &&
+          <div className={cx('header-mobile-left')}>
+            <div className={cx('header-mobile-lang-selector-container')}>
+              <LangSelector isShowPadding={false} />
+            </div>
+            <NavButton isOpen={isMenuOpen} onClick={handleNavClick} />
+          </div>
+        }
+      </div>
+      {isMobile &&
+        <nav className={cx('header-mobile-menu', isMenuOpen && 'is-open')}>
+          <ul>
+            {blockTitleList.map(blockObj =>
+              <li key={blockObj.key} onClick={handleMenuItemClick(blockObj.key)}>
+                <FormattedMessage id={blockObj.intlId} />
+              </li>
+            )}
+          </ul>
+        </nav>
+      }
+    </header>
+  );
 }
+
+Header.defaultProps = {
+  isHeaderFixed: false,
+}
+
+export default Header;
